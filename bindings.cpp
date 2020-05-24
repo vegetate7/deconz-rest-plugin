b5e9fcaacfb91d689e3800d1f1d373265f6b388d
Smanar <aurelien.pra@laposte.net>
2020-05-24 18:30:34 +0200

    Update bindings.cpp

--- bindings.cpp
+++ bindings.cpp
@@ -2512,7 +2512,7 @@ bool DeRestPluginPrivate::checkSensorBindingsForClientClusters(Sensor *sensor)
     {
         clusters.push_back(ONOFF_CLUSTER_ID);
         clusters.push_back(LEVEL_CLUSTER_ID);
-        clusters.push_back(SCENE_CLUSTER_ID);
+        clusters.push_back(COLOR_CLUSTER_ID);
         srcEndpoints.push_back(0x01);
         srcEndpoints.push_back(0x02);
         srcEndpoints.push_back(0x03);
